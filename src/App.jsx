eda1448d27ba4ef5a37c49daca8988ac4ad55dd0
Saavan Shah <s.saavan@wustl.edu>
2026-03-30 14:05:21 -0500

    finished status display

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,19 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  
+  
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -32,6 +42,7 @@ export default function Board() {
 
   return (
   <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
